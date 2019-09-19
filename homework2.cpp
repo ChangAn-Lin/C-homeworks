7b7c8ca005c1d24727fdaf839f4410b9e3b68ba0
ChangAn-Lin <54621598+ChangAn-Lin@users.noreply.github.com>
2019-09-19 23:15:42 +0800

    Add files via upload

---
+++ homework2.cpp
@@ -0,0 +1,80 @@
+#include<math.h>
+#include<stdio.h>
+#include<iostream>
+using namespace std;
+int numsolve()
+{
+	float a = 0, b = 0, c = 0, x1 = 0, x2 = 0,s,i;
+	cout << "请输入待求方程系数：";
+	cin >> a >> b >> c;
+	s = b * b - 4 * a * c;
+	i = sqrt(s);
+	if (s < 0)
+	{
+		cout << "无解"<<endl;
+
+
+	}
+	else if (s = 0)
+	{
+		x1 = ( - b) / (2 * a);
+		cout << "两个相等的根" << x1;
+	}
+	else
+	{
+		x1 = (i - b) / (2 * a);
+		x2 = (-i - b) / (2 * a);
+		cout << "两个根" << x1 <<" "<< x2<<endl;
+	}
+	return 0;
+}
+int trainglespace()
+{
+	float a, b, c, d,s;
+	cout << "请输入所求三角形的三边长度" << endl;
+	cin >> a >> b >> c;
+	d = (a + b + c) / 2;
+	s = sqrt(d*(d - a)*(d - b)*(d - c));
+	cout << s<<endl;
+	return 0;
+}
+
+int date()
+{
+	int year, month, day, days = 0,j=0;
+	int flag;
+	int a[7] = { 1,3,5,7,8,10,12 };
+	cout << "请输入按顺序输入要查询的年份、月份和日期：" << endl;
+	cin >>year >> month >> day;
+	for (int i = 1; i <month; i++)
+	{
+		if (i == 2)
+		{
+			if (year % 4 == 0)
+				days += 29;
+			else
+				days += 28;
+		}
+		else if (i == a[j])
+		{
+			days += 31;
+			j++;
+		}
+		else
+			days += 30;
+	}
+	
+	days = days + day;
+	cout << "这是今年第" << days << "天"<<endl;
+	cout << "这是今年第" << days/7 +1<< "周" << endl;
+	getchar();
+	return 0;
+}
+int main()
+{
+    numsolve();
+	trainglespace();
+	date();
+	getchar();
+	return 0;
+}
